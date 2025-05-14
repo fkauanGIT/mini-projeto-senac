cf0b71d7ca29e3ec0f783c1bf19ea816da5e2660
Lui7z <luizfm727@gmail.com>
2025-05-14 19:53:57 -0300

    Adição do Campo Nome no Cadastro

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
 session_start();
 
+$usuarioAdmin = [
+    "nome" => "admin",
+    "email" => "admin@senac.com",
+    "senha" => "1234"
+];
+
 $usuarioPadrao = [
+    "nome" => "teste",
     "email" => "teste@senac.com",
     "senha" => "1234"
 ];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
-    if ($email == $usuarioPadrao["email"] && $senha == $usuarioPadrao["senha"]) {
-        $_SESSION["usuario"] = $email;
+    if ($nome == $usuarioPadrao["nome"] && $email == $usuarioPadrao["email"] && $senha == $usuarioPadrao["senha"]) {
+        $_SESSION["usuario"] = $nome;
 
         if (isset($_POST["lembrar"])) {
             setcookie("email", $email, time() + 3600 * 24 * 30);
@@ -20,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: dashboard.php");
         exit;
     } else {
-        $erro = "Email ou senha inválidos!";
+        $erro = "Informações inválidas!";
     }
 }
 
@@ -28,15 +36,18 @@ $emailSalvo = $_COOKIE["email"] ?? "";
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Login</title></head>
+<head>
+    <title>Login</title>
+</head>
 <body>
-<h2>Login</h2>
-<?php if (isset($erro)) echo "<p style='color:red;'>$erro</p>"; ?>
-<form method="POST">
-    Email: <input type="email" name="email" value="<?= $emailSalvo ?>"><br>
-    Senha: <input type="password" name="senha"><br>
-    <label><input type="checkbox" name="lembrar"> Lembrar-me</label><br>
-    <button type="submit">Entrar</button>
-</form>
+    <h2>Login</h2>
+    <?php if (isset($erro)) echo "<p style='color:red;'>$erro</p>"; ?>
+    <form method="POST">
+        Nome: <input type="nome" name="nome"><br>
+        Email: <input type="email" name="email" value="<?= $emailSalvo ?>"><br>
+        Senha: <input type="password" name="senha"><br>
+        <label><input type="checkbox" name="lembrar"> Lembrar-me</label><br>
+        <button type="submit">Entrar</button>
+    </form>
 </body>
 </html>
