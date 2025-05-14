f364c248a5b77a239d16f89d35062f6abf2b8d63
José Aparecido dos Santos Silva <aparecidossd@gmail.com>
2025-05-14 20:26:16 -0300

    Mudando a aparência da aplicação

--- index.php
+++ index.php
@@ -28,15 +28,22 @@ $emailSalvo = $_COOKIE["email"] ?? "";
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Login</title></head>
+<head>
+    <link rel="stylesheet" href="assets/bonitamento.css">
+    <title>Login</title></head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <body>
-<h2>Login</h2>
-<?php if (isset($erro)) echo "<p style='color:red;'>$erro</p>"; ?>
-<form method="POST">
-    Email: <input type="email" name="email" value="<?= $emailSalvo ?>"><br>
-    Senha: <input type="password" name="senha"><br>
-    <label><input type="checkbox" name="lembrar"> Lembrar-me</label><br>
-    <button type="submit">Entrar</button>
-</form>
+    <div class="style">
+        <h2>Login</h2>
+        <?php if (isset($erro)) echo "<p style='color:red;'>$erro</p>"; ?>
+        <form method="POST">
+            Email: <input type="email" name="email" value="<?= $emailSalvo ?>"><br>
+            Senha: <input type="password" name="senha"><br>
+            <label><input type="checkbox" name="lembrar" id="check"> Lembrar-me</label><br>
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
 </body>
 </html>
