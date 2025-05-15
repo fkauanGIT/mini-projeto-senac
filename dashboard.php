aabd35b3b7311020da6d97665e352842342ec153
José Aparecido dos Santos Silva <aparecidossd@gmail.com>
2025-05-14 21:44:37 -0300

    mais alterações

--- dashboard.php
+++ dashboard.php
@@ -10,8 +10,13 @@ if (!isset($_SESSION["usuario"])) {
 <head>
     <title>Home</title>
     <link rel="stylesheet" href="assets/index.css">
+    <link rel="stylesheet" href="assets/dashboard.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <script src="scripts/local.js" defer></script>
     <script src="scripts/salvar-produto.js" defer></script>
+
 </head>
 <body>
 
@@ -55,17 +60,21 @@ if (!isset($_SESSION["usuario"])) {
     </div>
   </div>
 </section>
+<!-- conteudo da pagina -->
+ <div class="container">
+      <h2>Bem-vindo, <?= $_SESSION["usuario"] ?>!</h2>
+        <div class="cadastro">
+          <h3>Cadastrar Produto</h3>
+            <form id="form-produto">
+              Nome: <input type="text" id="nome"><br>
+              Preço: <input type="number" id="preco"><br>
+              <button type="submit">Salvar</button>
+             </form>
 
-<h2>Bem-vindo, <?= $_SESSION["usuario"] ?>!</h2>
+          <hr>
 
-<h3>Cadastrar Produto</h3>
-<form id="form-produto">
-    Nome: <input type="text" id="nome"><br>
-    Preço: <input type="number" id="preco"><br>
-    <button type="submit">Salvar</button>
-</form>
-
-<hr>
-<a href="vitrine.html">Ver Vitrine</a>
+        <a href="vitrine.html">Ver Vitrine</a>
+      </div>
+    </div>
 </body>
 </html>
